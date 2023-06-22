e3b5a24fb9ee6741ca4281342e4cab816180a6f2
Visagan Shan <visagan.shan@gmail.com>
2023-06-22 14:28:51 -0400

    added images to workouts

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const cors = require('cors');
 require("dotenv").config();
 const PORT = process.env.PORT || 5050;
 
+
+app.use(express.json());
 app.use(cors());
+app.use('/images', express.static('./public/images'));
 
 const workoutsRoutes = require('./routes/workout-routes');
 const exercisesRoutes = require('./routes/exercise-routes');
 
-app.use(express.json());
 // all users routes
 app.use('/workouts', workoutsRoutes);
 app.use('/exercises', exercisesRoutes)
