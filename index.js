1a869f6c3e004a85e88064437318a4db7f3fac74
Visagan Shan <visagan.shan@gmail.com>
2023-07-11 17:15:23 -0400

    connected database to aws

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require("dotenv").config();
-const PORT = process.env.PORT || 5050;
+const PORT = 8080 || 5050;
 
 app.use(express.json());
 app.use(cors());
@@ -19,6 +19,6 @@ app.use('/exercises', exercisesRoutes);
 app.use('/likes', likesRoutes);
 
 app.listen(PORT, () => {
-  console.log(`running at http://localhost:${PORT}`);
+  console.log(`running at localhost:${PORT}`);
 });
 
--- knexfile.js
+++ knexfile.js
@@ -13,6 +13,7 @@ module.exports = {
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    port: process.env.PORT,
     charset: "utf8",
   },
 };
